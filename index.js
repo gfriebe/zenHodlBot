2cc6b9d840a1c83d5747dcd729a4f5057f60db37
Gordon Friebe <gfriebe@primecoder.de>
2019-05-01 17:02:54 +0200

    some doc updates

--- index.js
+++ index.js
@@ -11,17 +11,16 @@ app.use(commandParts());
 let store = require("./storage");
 
 app.command('start', ctx => {
-  text =
+  const text =
     `Welcome to the zen way of hodling crypto!
 
-	(This is in alpha and currently not fully functionl. No expectations for norhing.)
-
 	If the following statements are true for you, you might like my services.
-	You are holding crypto.
-	You are interested in long term gains rather than short term trading.
-	You don't intend to sell or buy based on short term volatility.
-	You are checking the current value of your coins and portfolio several/many times a day, starting early morning.
-	This takes up time and energy and based on the marekt direction it makes you excited or frustrated.
+	
+	- You are holding crypto.
+	- You are interested in long term gains rather than short term trading.
+	- You don't intend to sell or buy based on short term volatility.
+	- You are checking the current value of your coins and portfolio several/many times a day, starting early morning.
+	- This takes up time and energy and based on the market direction it makes you excited or frustrated.
 
 	If you are a long term hodler, checking crypto on a daily basis is a distraction from your real life, from what matters.
 	It shouldn't have any influence on your emotional state and it shouldn't take up your time.
@@ -30,10 +29,10 @@ app.command('start', ctx => {
 	But still you want to know when something significant happens, something that might trigger an action from your part.
 
 	Here is where I come in by enabling you to:
-	- set price points on multi coins and I will send you a message once that price point is exceeded or fallen below
+	- set price points on multiple coins and I will send you a message once that price point is exceeded or fallen below
 	- manually ask me about the state of your coins and I will tell you the broad ranges based on your price points
 
-	From now on you will have the peace of mind of knowing you wont miss out on important changes while have in a general idea of the state of the market.
+	From now on you will have the peace of mind of knowing you won't miss out on important changes, while having a general idea of the state of the market.
 	
 	So, set up your prices, remove all other direct crypto information channels from phone and laptop and live your life more meaningful and happy ever after.
 	Type /help to get started!
@@ -41,31 +40,33 @@ app.command('start', ctx => {
   return ctx.reply(text)
 });
 
-app.command('help', ctx => {
-  var help =
+app.command(['help','h'], ctx => {
+  const help =
     `/start 				- Show the Welcome message
-	/add coin price			- Adds an alert price for this coin (I will send an alert once the value exceeds or falls below this price)
-	/remove coin [price] 	- Removes coin or a specific price (if given)
-	/list					- Show all coins and its alert prices 
-	/state        - current broad values for your coins					
-	/help					- Show this help
+/add coin price			- Adds an alert price for this coin (I will send an alert once the value exceeds or falls below this price)
+/remove coin [price] 	- Removes coin or a specific price (if given)
+/list					- Show all coins and its alert prices
+/state        - current broad values for your coins
+/help					- Show this help
+    
+You can also just use the first letter like /s for state.
 `
   return ctx.reply(help)
 });
 
-app.command('rate', async ctx => {
+app.command(['rate','r'], async ctx => {
   let coin = ctx.state.command.splitArgs[0];
   let resp = await store.getRateFor(coin)
 
   return ctx.reply(resp || 'there is no such coin');
 });
 
-app.command('list', async ctx => {
+app.command(['list','l'], async ctx => {
   resp = await store.list(ctx.from.id);
   ctx.reply(resp);
 });
 
-app.command('add', async ctx => {
+app.command(['add','a'], async ctx => {
   let args = ctx.state.command.splitArgs;
 
   if (!args[1]) {
@@ -84,7 +85,7 @@ app.command('add', async ctx => {
 
 });
 
-app.command('remove', async ctx => {
+app.command(['remove','r'], async ctx => {
   var args = ctx.state.command.splitArgs;
 
   if (!args[0]) {
@@ -101,7 +102,7 @@ app.command('remove', async ctx => {
 
 });
 
-app.command('status', async ctx => {
+app.command(['state', 's'], async ctx => {
 
   const coins = await store.list(ctx.from.id);
   let msg = ''
